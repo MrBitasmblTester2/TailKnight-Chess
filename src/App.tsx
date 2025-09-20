92103595c80e40fc47c70f02c99a4f1e883059ce
MrBitasmblTester2 <lulu2214@proton.me>
2025-09-20 22:45:24 +0400

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+import Game from './components/Game';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
