25e93503345ca611cd28f85501c1247c9b98ce60
MrBitasmblTester2 <lulu2214@proton.me>
2025-09-20 22:45:27 +0400

    Add gameLogic.ts

---
+++ src/utils/gameLogic.ts
@@ -0,0 +1,8 @@
+// src/utils/gameLogic.ts
+export function initBoard() {
+  // return 8x8 array with initial piece positions
+}
+
+export function movePiece(board, from, to) {
+  // shallow copy board and swap positions
+} 
